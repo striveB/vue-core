d699bfe3b1be6753d7bcd4e35455c2518b852ce7
jun233s <1227491415@qq.com>
2023-07-13 21:57:31 +0800

    复习：改造桶结构，封装track和trigger

--- review/main.js
+++ review/main.js
@@ -11,25 +11,54 @@ function effect(fn) {
   fn();
 }
 
-// 存储副作用函数的桶
-const bucket = new Set();
+// 存储副作用函数的桶，数据结构：WeakMap<target, Map<key, Set()<effects>>>
+const bucket = new WeakMap();
 
 // 对原始数据进行代理
 const obj = new Proxy(data, {
   // 拦截读取操作
   get(target, key) {
-    // 将副作用函数effect存入桶中
-    bucket.add(activeEffect);
+    track(target, key);
     return target[key];
   },
   // 拦截设置操作
   set(target, key, value) {
     target[key] = value;
-    // 执行桶中的副作用函数
-    bucket.forEach((effect) => effect());
+    trigger(target, key);
     return true;
   },
 });
+
+// 收集副作用
+function track(target, key) {
+  // 没有activeEffect说明不是在effect函数中读取数据，直接返回数据
+  if (!activeEffect) return target[key];
+  // 根据target从桶中拿到despMap，他也是一个Map类型，结构：key -> Set()<effects>
+  let depsMap = bucket.get(target);
+  // 如果没有despMap，说明是第一次读取数据，初始化一个despMap
+  if (!depsMap) {
+    bucket.set(target, (depsMap = new Map()));
+  }
+  // 根据key从despMap中拿到effects，他也是一个Set类型，结构：Set()<effects>
+  let effects = depsMap.get(key);
+  // 如果没有effects，说明是第一次读取数据，初始化一个effects
+  if (!effects) {
+    depsMap.set(key, (effects = new Set()));
+  }
+  // 将副作用函数effect存入effects中
+  effects.add(activeEffect);
+}
+
+// 触发副作用
+function trigger(target, key) {
+  // 根据target从桶中拿到despMap，他也是一个Map类型，结构：key -> Set()<effects>
+  let depsMap = bucket.get(target);
+  if (!depsMap) return true;
+  // 根据key从despMap中拿到effects，他也是一个Set类型，结构：Set()<effects>
+  let effects = depsMap.get(key);
+  // 执行桶中的副作用函数
+  effects && effects.forEach((effect) => effect());
+}
 
 effect(() => {
   document.body.innerText = obj.text;
